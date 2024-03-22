0ea5d2eb2985b1d13a7e2c49528af147e002c5c5
fiskinafiulafida <fiskinafiul@gmail.com>
2024-03-23 02:30:38 +0700

    View Dahboard Admin Fixs

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -157,60 +157,33 @@
 
 @section('row2')
 <!-- Area Chart -->
-<div class="col-lg-6 mb-4">
+<div class="col-lg-12 mb-4">
     <div class="card shadow mb-4">
         <!-- Card Header - Dropdown -->
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
             <h6 class="m-0 font-weight-bold text-primary">Produksi Telur Ayam</h6>
             <div class="dropdown no-arrow">
-                <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                    <div class="dropdown-header">Dropdown Header:</div>
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                <div class="col-xl-3 col-md-3 mb-9">
+                    <select name="tahunProduksi_id" id="tahunProduksi_id" class="btn btn-light @error('tahunProduksi_id') is-invalid @enderror" value="{{ old('tahunProduksi_id') }}" data-toggle="dropdown" style="width:650px">
+                        <option value="">-- Pilih Kandang Ayam --</option>
+
+                    </select>
+                </div>
+            </div>
+            <div class="dropdown no-arrow">
+                <div class="col-xl-10 mb-9">
+                    <select name="tahunProduksi_id" id="tahunProduksi_id" class="btn btn-light @error('tahunProduksi_id') is-invalid @enderror" value="{{ old('tahunProduksi_id') }}" data-toggle="dropdown" style="width:650px">
+                        <option value="">-- Pilih Tahun Produksi --</option>
+
+                    </select>
                 </div>
             </div>
         </div>
+
         <!-- Card Body -->
         <div class="card-body">
             <div class="chart-area">
                 <canvas id="myAreaChart"></canvas>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Content Column -->
-<div class="col-lg-6 mb-4">
-
-    <!-- Project Card Example -->
-    <div class="card shadow mb-4">
-        <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Brown's Double Exponential Smoothing</h6>
-        </div>
-        <div class="card-body">
-            <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-            <div class="progress mb-4">
-                <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-            <div class="progress mb-4">
-                <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-            <div class="progress mb-4">
-                <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-            <div class="progress mb-4">
-                <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-            <div class="progress">
-                <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
     </div>
